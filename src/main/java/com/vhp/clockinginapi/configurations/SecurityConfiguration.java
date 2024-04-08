edf79f7ce9bd100f7a30b930e266d57a1e0d8c98
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-04-08 13:54:14 -0300

    feat: authorization by role

--- src/main/java/com/vhp/clockinginapi/configurations/SecurityConfiguration.java
+++ src/main/java/com/vhp/clockinginapi/configurations/SecurityConfiguration.java
@@ -4,30 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.vhp.clockinginapi.configurations.jwt.AuthenticateFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
   
-  private static final String[] PERMITED_LIST = { "**" };
+  private static final String[] PERMITED_LIST = { "/authenticate" };
 
   private final AuthenticationProvider authenticationProvider;
+  private AuthenticateFilter authenticateFilter;
 
-  public SecurityConfiguration(AuthenticationProvider authenticationProvider) {
+  public SecurityConfiguration(AuthenticationProvider authenticationProvider, AuthenticateFilter authenticateFilter) {
       this.authenticationProvider = authenticationProvider;
+      this.authenticateFilter = authenticateFilter;
   }
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
       .csrf(crsf -> crsf.disable())
-      .authorizeHttpRequests(req -> req.requestMatchers(PERMITED_LIST)
-              .permitAll()
-              .anyRequest()
-              .permitAll())
+      .authorizeHttpRequests(req -> req.requestMatchers(PERMITED_LIST).permitAll()
+              .anyRequest().authenticated())
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-      .authenticationProvider(authenticationProvider);
+      .authenticationProvider(authenticationProvider)
+      .addFilterBefore(authenticateFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
