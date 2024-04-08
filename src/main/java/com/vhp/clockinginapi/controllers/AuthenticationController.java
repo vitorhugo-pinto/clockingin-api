5bcc14f651c960efe47e80c435d583126a75c3d7
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-04-08 12:46:30 -0300

    feat: controller to authenticate user

---
+++ src/main/java/com/vhp/clockinginapi/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.vhp.clockinginapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vhp.clockinginapi.dtos.ApiResponseDTO;
+import com.vhp.clockinginapi.dtos.AuthenticationRequestDTO;
+import com.vhp.clockinginapi.dtos.AuthenticationResponseDTO;
+import com.vhp.clockinginapi.services.AuthenticationService;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponseDTO<AuthenticationResponseDTO>> authenticate(@RequestBody AuthenticationRequestDTO request) {
+        return ResponseEntity.ok(
+                new ApiResponseDTO<>(
+                        true,
+                        "Authentication completed successfully",
+                        authenticationService.authenticate(request),
+                        null));
+    }
+}
+
